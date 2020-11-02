f8e8e0420e745118d513e7f15f0a8e297a49b8dd
aungayethan <aungayethan2015@gmail.com>
2020-11-02 11:19:51 +0630

    added new metric to record duration in cloudwatch:

--- utils/enum.go
+++ utils/enum.go
@@ -5,5 +5,7 @@ package utils
 
 const MetricName string = "Site Availability"
 const WorkerLimit int   = 250
+const MetricStatusNameSpace string = "Website Status"
+const MetricDurationNameSpace string = "Duration"
 
 
